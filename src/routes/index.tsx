8557e0a802d02fffff248e3059790bc9f5090275
Carlos Roberto De Rolt Junior <carlosroltjr@gmail.com>
2021-05-23 23:30:29 -0300

    feat: register and dashboard routes

--- src/routes/index.tsx
+++ src/routes/index.tsx
@@ -1,12 +1,16 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
-import { NotFound } from '../pages/Notfound'
 import { Login } from '../pages/Login'
+import { Register } from '../pages/Register'
+import { Dashboard } from '../pages/Dashboard'
+import { NotFound } from '../pages/Notfound'
 
 const Routes: React.FC = () => (
   <Switch>
     <Route path="/" exact component={ Login }/>
+    <Route path="/register" exact component={ Register }/>
+    <Route path="/dashboard" exact component={ Dashboard }/>
     <Route path="/*" component={ NotFound }/>
   </Switch>
 )
